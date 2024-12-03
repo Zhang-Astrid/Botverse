4d78cf0ee77cc53776b342e0dc0f3843844ad2d4
张善（Shan ZHANG） <134914111+Zhang-Astrid@users.noreply.github.com>
2024-12-03 21:07:28 +0800

    Add files via upload

--- src/views/search/components/Sidebar.vue
+++ src/views/search/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="sidebar">
     <ul>
-      <li @click="navigate('search')">搜索</li>
-      <li @click="navigate('ranking')">排行榜</li>
+      <li @click="navigate('search')">Search</li>
+      <li @click="navigate('ranking')">Rankings</li>
     </ul>
   </div>
 </template>
@@ -19,12 +19,14 @@ export default {
 
 <style scoped>
 .sidebar {
-  width: 80px;
-  background-color: #f7f7f7;
+  width: 100px;
+  background-color: #57565b;
   padding: 20px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  border-radius: 0px 8px 8px 8px; /* 圆角 */
+
 }
 
 .sidebar ul {
@@ -36,10 +38,13 @@ export default {
   padding: 15px;
   cursor: pointer;
   transition: background-color 0.3s;
+  justify-content: center;
+  color:white;
 }
 
 .sidebar li:hover {
-  background-color: #0066cc;
-  color: white;
+  background-color: rgb(238, 232, 170); /* 深蓝色 */
+  color: #ffffff;
+  border-radius: 8px; /* 圆角 */
 }
 </style>
