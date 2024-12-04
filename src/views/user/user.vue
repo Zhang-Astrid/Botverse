abb56219c7c72a288956772a91448e85d73e1b9d
张善（Shan ZHANG） <134914111+Zhang-Astrid@users.noreply.github.com>
2024-12-04 18:25:51 +0800

    Add files via upload

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,14 +1,14 @@
 <template>
+  <ul class="header-links">
+    <li><a :href="links.main" class="styled-link">返回主界面</a></li>
+    <li><a :href="links.login" class="styled-link">退出登录</a></li>
+  </ul>
   <el-container>
     <Sidebar @toggleView="switchView" />
     <el-main>
       <component :is="currentView" />
     </el-main>
   </el-container>
-  <ul>
-    <li><a :href="links.main" class="styled-link">返回主界面</a></li>
-    <li><a :href="links.login" class="styled-link">退出登录</a></li>
-  </ul>
 
 </template>
 
@@ -74,16 +74,29 @@ export default {
   margin: 0 auto;
   width: fit-content;
   font-size: 18px;
-  color: #333;
+  color: #4c5caf;
   font-family: Arial, sans-serif;
   text-decoration: none;
-  border-bottom: 2px solid #007BFF;
   padding: 10px 20px; /* 添加一些内边距 */
   transition: all 0.3s ease; /* 平滑过渡效果 */
 }
 
 .styled-link:hover {
-  color: #007BFF;
-  border-bottom: 2px solid #007BFF;
+  color: rgba(30, 41, 61, 0.56);
+}
+
+.header-links {
+  display: flex;
+  justify-content: flex-end; /* 让链接项靠右 */
+  position: fixed; /* 固定在页面顶部 */
+  top: 0;
+  right: 0;
+  margin: 20px; /* 距离右上角有些间隙 */
+  padding: 0;
+  list-style-type: none; /* 去掉默认的列表样式 */
+}
+
+.header-links li {
+  margin-left: 20px; /* 给每个链接之间加点间隔 */
 }
 </style>
