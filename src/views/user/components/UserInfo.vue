c7dfd0039acc6811d8f03af5691ac1e1788698c4
张善（Shan ZHANG） <134914111+Zhang-Astrid@users.noreply.github.com>
2024-12-04 16:35:25 +0800

    Add files via upload

--- src/views/user/components/UserInfo.vue
+++ src/views/user/components/UserInfo.vue
@@ -2,14 +2,18 @@
   <el-card class="box-card">
     <div slot="header" class="clearfix">
       <span>个人信息</span>
-      <el-button style="float: right;" type="primary" @click="toggleEdit" v-if="is_current">编辑</el-button>
-      <el-button style="float: right;margin-right: 10px;" type="primary" @click="openRecharge" v-if="is_current">充值</el-button>
+      <div class="button">
+        <el-button style="float: right;" type="primary" @click="toggleEdit" v-if="is_current">编辑</el-button>
+        <el-button style="float: right;margin-right: 10px;" type="primary" @click="openRecharge" v-if="is_current">
+          充值
+        </el-button>
+      </div>
     </div>
     <div v-if="!editMode" class="text item">
       <!-- 非编辑模式下的只读信息展示 -->
       <el-form label-width="80px">
         <el-form-item label="头像">
-          <img v-if="userInfo.image" :src="userInfo.image" class="avatar" />
+          <img v-if="userInfo.image" :src="userInfo.image" class="avatar"/>
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-form-item>
         <el-form-item label="UID">{{ userInfo.user_id }}</el-form-item>
@@ -23,8 +27,8 @@
       <!-- 编辑模式下的表单 -->
       <el-form ref="dataForm" :model="userInfo" label-width="80px">
         <el-form-item label="头像">
-          <input type="file" @change="handleFileChange" />
-          <img v-if="userInfo.image" :src="userInfo.image" class="avatar"  alt="图像预览"/>
+          <input type="file" @change="handleFileChange"/>
+          <img v-if="userInfo.image" :src="userInfo.image" class="avatar" alt="图像预览"/>
         </el-form-item>
         <el-form-item label="性别">
           <el-select v-model="userInfo.gender" placeholder="请选择">
@@ -34,20 +38,20 @@
           </el-select>
         </el-form-item>
         <el-form-item label="生日">
-         <el-date-picker
-        v-model="userInfo.birthday"
-        type="date"
-        placeholder="Pick a day"
-        />
+          <el-date-picker
+              v-model="userInfo.birthday"
+              type="date"
+              placeholder="Pick a day"
+          />
         </el-form-item>
-<!--        <el-form-item label="积分">-->
-<!--          <el-input type="textarea" v-model="userInfo.score" />-->
-<!--        </el-form-item>-->
+        <!--        <el-form-item label="积分">-->
+        <!--          <el-input type="textarea" v-model="userInfo.score" />-->
+        <!--        </el-form-item>-->
         <el-form-item label="旧密码">
-          <el-input type="password" v-model="userInfo.old_password" />
+          <el-input type="password" v-model="userInfo.old_password"/>
         </el-form-item>
         <el-form-item label="新密码">
-          <el-input type="password" v-model="userInfo.new_password" />
+          <el-input type="password" v-model="userInfo.new_password"/>
         </el-form-item>
 
         <el-form-item>
@@ -59,15 +63,15 @@
   </el-card>
 
   <!-- 编辑模式下的弹出窗口 -->
-  <el-dialog v-model="dialogVisible" title="充值">
+  <el-dialog v-model="dialogVisible" title="充值" class="inside-class">
     <el-form :model="userInfo" label-width="80px">
       <el-form-item label="输入充值金额" prop="increament">
         <el-input
-          type="textarea"
-          v-model.number="userInfo.increament"
-          @input="handleInput"
-          autocomplete="off"
-          maxlength="9"
+            type="textarea"
+            v-model.number="userInfo.increament"
+            @input="handleInput"
+            autocomplete="off"
+            maxlength="9"
         />
       </el-form-item>
     </el-form>
@@ -81,6 +85,7 @@
 <script>
 import axios from "axios";
 import {mapActions, mapGetters} from 'vuex';
+
 export default {
   computed: {
     ...mapGetters(['getSharedData']),
@@ -93,21 +98,21 @@ export default {
       dialogVisible: false,
       rules: {
         increament: [
-          { required: true, message: '请输入充值金额', trigger: 'blur' },
-          { pattern: /^[1-9]\d*$/, message: '请输入一个正整数', trigger: 'blur' }
+          {required: true, message: '请输入充值金额', trigger: 'blur'},
+          {pattern: /^[1-9]\d*$/, message: '请输入一个正整数', trigger: 'blur'}
         ]
       },
       userInfo: {
-        user_id:'1',
+        user_id: '1',
         username: '',
         gender: 'male',
         birthday: '2004-1-1',
         score: '50',
         image: 'https://multiavatar.com/img/logo-animated.gif?v=003', // 用户头像的URL
-        password:'123456',
-        old_password:'',
-        new_password:'',
-        increament:'',
+        password: '123456',
+        old_password: '',
+        new_password: '',
+        increament: '',
 
       }
     };
@@ -134,7 +139,7 @@ export default {
     toggleEdit() {
       this.editMode = !this.editMode;
     },
-    openRecharge(){
+    openRecharge() {
       this.dialogVisible = true;
     },
     async getUserData() {
@@ -161,17 +166,17 @@ export default {
     async submitForm() {
       this.$message('提交表单');
       try {
-        console.log("Info",this.userInfo)
+        console.log("Info", this.userInfo)
         const response = await axios.post('http://127.0.0.1:8080/user_sys/update', this.userInfo);
         // alert(response.status);
-        
+
       } catch (error) {
 
-          // 后端返回的错误消息
-          // errorMessage.value = error.response.data.message;
-          alert(error.response.data.message);
-          
-          //把数据改成旧数据
+        // 后端返回的错误消息
+        // errorMessage.value = error.response.data.message;
+        alert(error.response.data.message);
+
+        //把数据改成旧数据
       }
       this.getUserData()
       this.editMode = !this.editMode;
@@ -185,7 +190,7 @@ export default {
         if (response.status === 200) {
           alert(response.data.message); // 显示成功消息
           const new_info = await axios.post('http://127.0.0.1:8080/user_sys/user',
-            this.userInfo
+              this.userInfo
           );
           // alert(JSON.stringify(new_info.data))
           this.userInfo.name = new_info.data.username;
@@ -198,10 +203,10 @@ export default {
         }
       } catch (error) {
 
-          // 后端返回的错误消息
-          // errorMessage.value = error.response.data.message;
-          alert(error.response.data.message);
-          //把数据改成旧数据
+        // 后端返回的错误消息
+        // errorMessage.value = error.response.data.message;
+        alert(error.response.data.message);
+        //把数据改成旧数据
       }
       this.dialogVisible = false;
     },
@@ -226,6 +231,27 @@ export default {
 </script>
 
 <style scoped>
+.box-card button{
+  background-color: #4c5caf;
+  color: #fff;
+}
+
+.box-card button:hover {
+  background-color: #eee8aa;
+  color: #4c5caf;
+}
+
+.inside-class button{
+  background-color: #4c5caf;
+  color: #fff;
+}
+
+.inside-class button:hover {
+  background-color: #eee8aa;
+  color: #4c5caf;
+}
+
+
 .text {
   font-size: 14px;
 }
